cbc4a0c05a6a870ba2ad00fc51caad83f081682c
aditya745 <thakuraditya72@yahoo.in>
2019-03-18 16:42:01 +0200

    font fixed

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -3,16 +3,16 @@ import contact from "../image/contact.jpg";
 const About = props => {
     return (
       <div className="about_me_container">
-        <h1>About Me</h1>
+        <h2>About Me</h2>
         <div className="contact-container">
         <div className = "contact-image">
           <img src={contact} alt="contact" style = {{width:"50%", height:"50%"}}/>
         </div>
         <div className = "contact-details">
-          <h3><strong>Name:</strong> Aditya Rana</h3>
-          <h3><strong>Email:</strong> adiran313u@gmail.com</h3>
-          <h3><strong>Address:</strong> yö-kylä 1C 11 Turku,Finland</h3>
-          <h3><strong>Tel:</strong> +358453581678</h3>
+          <h5><strong>Name:</strong> Aditya Rana</h5>
+          <h5><strong>Email:</strong> adiran313u@gmail.com</h5>
+          <h5><strong>Address:</strong> yö-kylä 1C 11 Turku,Finland</h5>
+          <h5><strong>Tel:</strong> +358453581678</h5>
         </div>
       </div>
       </div>
